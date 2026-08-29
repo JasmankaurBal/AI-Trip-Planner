b11d8a29eff95d72ec7a9f500b607284e52cca90
Jasmankaur Bal <jasmanbal13@gmail.com>
2026-08-29 11:51:19 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate , useloaction} from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import { LoadingState } from "./components/ui/states";
 import AppLayout from "./layouts/AppLayout";
